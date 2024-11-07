a5fe756dd2a59e63a54eb9dd905f1de7b4d3b886
ruhul <ruhulamin15jun1999@gmail.com>
2024-11-07 11:59:15 +0600

    problem 4 solved

---
+++ src/p4.ts
@@ -0,0 +1,22 @@
+type Circle = {
+  shape: "circle";
+  radius: number;
+};
+
+type Rectangle = {
+  shape: "rectangle";
+  width: number;
+  height: number;
+};
+
+type Shape = Circle | Rectangle;
+
+function calculateShapeArea(shape: Shape): number {
+  if (shape.shape === "circle") {
+    return parseFloat((Math.PI * Math.pow(shape.radius, 2)).toFixed(2));
+  } else if (shape.shape === "rectangle") {
+    // Area of a rectangle = width * height
+    return shape.width * shape.height;
+  }
+  throw new Error("thikthak input den");
+}
